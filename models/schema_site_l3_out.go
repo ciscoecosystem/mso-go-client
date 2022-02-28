cb0a9db398d28041f6aef2c80aa382b2a94152cb
RutvikS-crest <rutvik.shiyani@crestdatasys.com>
2022-02-28 13:42:16 +0530

    updated model and service for schema site l3out

--- models/schema_site_l3_out.go
+++ models/schema_site_l3_out.go
@@ -56,7 +56,7 @@ func DeleteIntersiteL3outsModel(l3out *IntersiteL3outs) *SiteL3Out {
 	return &site
 }
 
-func IntersiteL3outsFromContainer(cont *container.Container, tf IntersiteL3outs) (*IntersiteL3outs, error) {
+func IntersiteL3outsFromContainer(cont *container.Container, tf *IntersiteL3outs) (*IntersiteL3outs, error) {
 	remoteL3out := IntersiteL3outs{}
 	var found bool = false
 	count, err := cont.ArrayCount("sites")
@@ -70,22 +70,24 @@ func IntersiteL3outsFromContainer(cont *container.Container, tf IntersiteL3outs)
 		}
 		apiSite := StripQuotes(tempCont.S("siteId").String())
 		templateName := StripQuotes(tempCont.S("templateName").String())
-
 		if apiSite == tf.SiteId && templateName == tf.TemplateName {
 			l3outCount, err := tempCont.ArrayCount("intersiteL3outs")
 			if err != nil {
 				return nil, fmt.Errorf("unable to get l3out list")
 			}
+			l3outCont := tempCont.S("intersiteL3outs")
 			for j := 0; j < l3outCount; j++ {
-				l3outRef := strings.Split(StripQuotes(tempCont.S("l3outRef").String()), "/")
+				l3outTempCont := l3outCont.Index(j)
+				l3outRef := strings.Split(StripQuotes(l3outTempCont.S("l3outRef").String()), "/")
 				l3outName := l3outRef[len(l3outRef)-1]
-				vrfRef := strings.Split(StripQuotes(tempCont.S("vrfRef").String()), "/")
+				vrfRef := strings.Split(StripQuotes(l3outTempCont.S("vrfRef").String()), "/")
 				vrfName := vrfRef[len(vrfRef)-1]
 				if l3outName == tf.L3outName && vrfName == tf.VRFName {
 					remoteL3out.L3outName = l3outName
 					remoteL3out.VRFName = vrfName
 					remoteL3out.SchemaID = l3outRef[2]
 					remoteL3out.SiteId = apiSite
+					remoteL3out.TemplateName = templateName
 					found = true
 					break
 				}
