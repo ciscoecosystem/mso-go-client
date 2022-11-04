51442818efeeb45bb3c9a574ad05777f29cdc442
akinross <akinross@cisco.com>
2022-11-04 16:02:25 +0100

    [minor change] dhcp labels

--- models/model.go
+++ models/model.go
@@ -3,3 +3,19 @@ package models
 type Model interface {
 	ToMap() (map[string]interface{}, error)
 }
+
+type patchPayload struct {
+	Ops   string                 `json:",omitempty"`
+	Path  string                 `json:",omitempty"`
+	Value map[string]interface{} `json:",omitempty"`
+}
+
+func (patchPayloadAttributes *patchPayload) ToMap() (map[string]interface{}, error) {
+	patchPayloadAttributesMap := make(map[string]interface{})
+	A(patchPayloadAttributesMap, "op", patchPayloadAttributes.Ops)
+	A(patchPayloadAttributesMap, "path", patchPayloadAttributes.Path)
+	if patchPayloadAttributes.Value != nil {
+		A(patchPayloadAttributesMap, "value", patchPayloadAttributes.Value)
+	}
+	return patchPayloadAttributesMap, nil
+}
