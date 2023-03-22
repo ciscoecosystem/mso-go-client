199bb8f50ee5d7046b9b4ec8a351edbaf42bcd41
anvitha jain <anvjain@cisco.com>
2023-03-22 12:59:30 -0700

    [minor_changes] Added PatchPayloadList for values that are in List([]interface{}) form

--- models/schema_site_anp_epg_bulk_staticport.go
+++ models/schema_site_anp_epg_bulk_staticport.go
@@ -1,28 +1,11 @@
 package models
 
-type SchemaSiteAnpEpgBulkStaticPort struct {
-	Ops   string        `json:",omitempty"`
-	Path  string        `json:",omitempty"`
-	Value []interface{} `json:",omitempty"`
-}
+func NewSchemaSiteAnpEpgBulkStaticPort(ops, path string, staticPortsList []interface{}) *PatchPayloadList {
 
-func NewSchemaSiteAnpEpgBulkStaticPort(ops, path string, staticPortsList []interface{}) *SchemaSiteAnpEpgBulkStaticPort {
-
-	return &SchemaSiteAnpEpgBulkStaticPort{
+	return &PatchPayloadList{
 		Ops:   ops,
 		Path:  path,
 		Value: staticPortsList,
 	}
 
-}
-
-func (anpAttributes *SchemaSiteAnpEpgBulkStaticPort) ToMap() (map[string]interface{}, error) {
-	anpAttributesMap := make(map[string]interface{})
-	A(anpAttributesMap, "op", anpAttributes.Ops)
-	A(anpAttributesMap, "path", anpAttributes.Path)
-	if anpAttributes.Value != nil {
-		A(anpAttributesMap, "value", anpAttributes.Value)
-	}
-
-	return anpAttributesMap, nil
 }
